3bcef79d65716ea1937f453cc7d4bf67cb8bb145
Dhiraj <Dhiraj@DhirajPC>
2018-12-31 00:09:36 +0530

    removed switch case from Initiliazebrowserdriver class as it was returing NULL. neew to work more on it

--- BorlandInsurace_TestNG/src/utilities/InitilizeBrowserDriver.java
+++ BorlandInsurace_TestNG/src/utilities/InitilizeBrowserDriver.java
@@ -1,12 +1,9 @@
 package utilities;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class InitilizeBrowserDriver {
 	
@@ -16,25 +13,23 @@ public class InitilizeBrowserDriver {
 	{
 		
 		//String TestRunningon;
-		WebDriver driver = null;
-		Properties prop = new Properties();
-		FileInputStream fs = new FileInputStream("E:\\Selenium\\projects\\BorlandInsurace_TestNG\\BrowserInit.properties");
-		prop.load(fs);
-		
-		
-		switch (prop.getProperty("Browser").toLowerCase())
+		WebDriver driver;
+		//Properties prop = new Properties();
+		//FileInputStream fs = new FileInputStream("E:\\Selenium\\projects\\BorlandInsurace_TestNG\\BrowserInit.properties");
+		//prop.load(fs);
+		/*switch (prop.getProperty("Browser").toLowerCase())
 		{
-		case "chrome":
+		case "chrome":*/
 			System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 			driver = new ChromeDriver();	
-			break;
+			/*break;
 		case "mozilla":
 			System.setProperty("webdriver.gecko.driver","E:\\Selenium\\geckodriver-v0.23.0-win32\\geckodriver.exe");
 			driver = new FirefoxDriver();;	
 			break;
 		default:
 			break;
-		}
+		}*/
         return driver;
 		
 		
