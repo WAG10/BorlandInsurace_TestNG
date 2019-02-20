1af908fafed9cd6cece828cca062a7ca39e612a7
Dhiraj <Dhiraj@DhirajPC>
2019-02-20 22:09:00 +0530

    Takescreenhsot class added in utility package Takescreenhsot also called for test cases

--- BorlandInsurace_TestNG/src/utilities/TakeScreeshot.java
+++ BorlandInsurace_TestNG/src/utilities/TakeScreeshot.java
@@ -9,12 +9,13 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class TakeScreeshot {
+	//added to facilitate sceenhpotd
 
 	public static void captureScreen(WebDriver driver,String FileNameWithPath)
 	{
 		
 File ScrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		
+		System.out.println("Hello screenshot is being taken");
 		try {
 			FileUtils.copyFile(ScrFile, new File("C:\\Users\\Dhiraj\\Desktop\\" + FileNameWithPath+".png"));
 		} catch (IOException e) {
